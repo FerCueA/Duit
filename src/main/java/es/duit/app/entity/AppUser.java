c534d0e66047603f31b60d7162ecb8570d4ee4b6
Alejo <fercuea90@protonmail.com>
2026-01-23 19:57:10 +0000

    Primer configuracon NEON + Permisos inciales

---
+++ src/main/java/es/duit/app/entity/AppUser.java
@@ -0,0 +1,20 @@
+package es.duit.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+}
