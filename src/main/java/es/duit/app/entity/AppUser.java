137f0dae9c427457d8f4396feb5716cf001fc0cd
Aleixo <fercuea90@protonmail.com>
2026-01-24 11:37:27 +0000

    probando

--- src/main/java/es/duit/app/entity/AppUser.java
+++ src/main/java/es/duit/app/entity/AppUser.java
@@ -11,10 +11,14 @@ import lombok.Data;
 @Entity
 @Table(name = "app_user")
 public class AppUser {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String username;
     private String password;
     private String role;
+    private String idrole;
 }
