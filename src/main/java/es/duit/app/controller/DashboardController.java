d6ec4e0c3e968e9d602968c05597684402dc050f
Aleixo <fercuea90@protonmail.com>
2026-01-24 10:07:28 +0000

    Home

--- src/main/java/es/duit/app/controller/DashboardController.java
+++ src/main/java/es/duit/app/controller/DashboardController.java
@@ -1,13 +1,26 @@
 package es.duit.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import es.duit.app.entity.AppUser;
+import es.duit.app.repository.AppUserRepository;
 
 @Controller
 public class DashboardController {
 
+    @Autowired
+    private AppUserRepository appUserRepository;
+
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        AppUser usuario = appUserRepository.findByUsername(username).orElse(null);
+        model.addAttribute("usuario", usuario);
         return "dashboard/home";
     }
 
